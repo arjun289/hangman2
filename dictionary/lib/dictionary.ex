f5fd6c59ff718ec158c8d82cd9277952d76207db
arjun289 <arjun289singh@gmail.com>
2022-01-30 06:19:49 +0800

    Initialize dictionary

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,13 @@
+defmodule Dictionary do
+  @moduledoc """
+  Documentation for `Dictionary`.
+  """
+  def word_list() do
+    words = File.read!("assets/words.txt")
+    String.split(words, ~r/\n/, trim: true)
+  end
+
+  def random_word() do
+    Enum.random(word_list())
+  end
+end
